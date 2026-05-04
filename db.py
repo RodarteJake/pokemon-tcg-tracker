da9a72318497cdcc9b5a404807cdc142c697a682
Jacob Rodarte <jacobarodarte@gmail.com>
2026-05-04 11:12:53 -0700

    fix: enable SQLite foreign key enforcement (#5)

--- db.py
+++ db.py
@@ -37,6 +37,7 @@ def get_connection():
     """Open a connection to the database with row_factory set."""
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
 
 
